7e8542a2248d3f57bf83c1edecc3bad77a171056
mgj <mgj@mgjs.dk>
2024-10-19 20:01:30 +0200

    init

---
+++ MyMauiHybridApp/MyMauiHybridApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MyMauiHybridApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
